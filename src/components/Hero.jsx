4995c7f857b85c97c6b040a0886d03d30a3fd2d1
SH-SharifAhamed <sharif845g7@gmail.com>
2026-05-03 12:14:55 +0600

    data update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,24 @@
 import BookCard from "./BookCard";
 
-
 const Hero = async () => {
-     
-     const res = await fetch(
-       "https://online-book-platform.vercel.app/data.json",
-     );
-     const books = await res.json();
-     // 4 books sliced from the top
-     const topBooks = books.slice(8, 13);
-     
+  const res = await fetch("https://online-book-platform.vercel.app/data.json", {
+    cache: "no-store",
+  });
+  const books = await res.json();
+  // 4 books sliced from the top
+  const topBooks = books.slice(8, 13);
 
-     return (
-       <div className="max-w-5xl w-full mx-auto mt-8 px-4">
-         <h1 className="text-2xl sm:text-3xl font-bold my-5">Popular Books</h1>
+  return (
+    <div className="max-w-5xl w-full mx-auto mt-8 px-4">
+      <h1 className="text-2xl sm:text-3xl font-bold my-5">Popular Books</h1>
 
-         <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-5">
-           {topBooks.map((book) => (
-             <BookCard key={book.id} book={book} />
-           ))}
-         </div>
-       </div>
-     );
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-5">
+        {topBooks.map((book) => (
+          <BookCard key={book.id} book={book} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
